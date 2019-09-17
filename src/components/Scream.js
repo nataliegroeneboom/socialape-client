5848fb7d10ba10ccfcfc157b90bffdb7820e6038
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-09-17 16:58:25 +0100

    add card details with material ui

---
+++ src/components/Scream.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import Link from 'react-router-dom/Link';
+import withStyles from '@material-ui/core/styles/withStyles';
+// material UI cards
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    card: {
+        display: 'flex',
+        marginBottom: 20,
+        marginRight: 20
+    },
+    image: {
+        minWidth: 200,
+        objectFit: 'cover'
+
+    },
+    content: {
+        padding: 25
+    }
+}
+class Scream extends Component {
+    render() {
+        const { classes, scream: {body, createdAt, userImage, userHandle, screamId, likeCount, commentCount} } = this.props;
+        return (
+            <div>
+                <Card className={classes.card}>
+                    <CardMedia image={userImage}
+                                className={classes.image}
+                                title="Profile image"/>
+                    <CardContent className ={classes.details}>
+                        <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
+                        <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+                        <Typography variant="body2">{body}</Typography>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(Scream);
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 
-export class Home extends Component {
+import Scream from '../components/Scream';
+
+class Home extends Component {
+    state = {
+        screams: null
+    }
+    componentDidMount(){
+        axios.get('/screams')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    screams: res.data
+                })
+            })
+            .catch(err => console.log(err))
+    }
     render() {
+        let recentScreamsMarkup = this.state.screams? (
+            this.state.screams.map(scream => <Scream scream={scream}/>)
+        ): <p>Loading...</p>;
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <Grid container spacing={16}>
+                <Grid item sm={8} xs={12}>
+                   {recentScreamsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                   <p>Profile</p>
+                </Grid>
+            </Grid>
         )
     }
 }
