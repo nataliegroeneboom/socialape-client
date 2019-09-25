39425929d1a09fb3646d690fbb625915b5ca87aa
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-09-25 15:13:55 +0100

    add post component

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -1,22 +1,58 @@
-import {SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM, SET_SCREAM, DELETE_SCREAM} from '../types';
+import {
+    SET_SCREAMS,
+    LOADING_DATA,
+    LIKE_SCREAM,
+    UNLIKE_SCREAM,
+    SET_SCREAM,
+    DELETE_SCREAM,
+    LOADING_UI,
+    POST_SCREAM, 
+    SET_ERRORS,
+    CLEAR_ERRORS
+} from '../types';
 import axios from 'axios';
 
 // Get all screams
 export const getScreams = () => dispatch => {
- dispatch({type: LOADING_DATA});
- axios.get('/screams')
-    .then(res => {
-        dispatch({
-            type: SET_SCREAMS,
-            payload: res.data
+    dispatch({
+        type: LOADING_DATA
+    });
+    axios.get('/screams')
+        .then(res => {
+            dispatch({
+                type: SET_SCREAMS,
+                payload: res.data
+            })
         })
-    })
-    .catch(err => {
-        dispatch({
-            type: SET_SCREAMS,
-            payload: []
+        .catch(err => {
+            dispatch({
+                type: SET_SCREAMS,
+                payload: []
+            })
         })
-    })
+}
+
+// Post a scream
+export const postScream = (newScream) => dispatch => {
+    dispatch({
+        type: LOADING_UI
+    });
+    axios.post('/scream', newScream)
+        .then(res => {
+            dispatch({
+                type: POST_SCREAM,
+                payload: res.data
+            })
+            dispatch({
+                type: CLEAR_ERRORS
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: SET_ERRORS,
+                payload: err.response.data
+            })
+        })
 }
 
 //Like a scream
@@ -30,7 +66,7 @@ export const likeScream = (screamId) => dispatch => {
         })
         .catch(err => console.log(err))
 }
- 
+
 // Unlike a scream
 export const unlikeScream = (screamId) => dispatch => {
     axios.get(`/scream/${screamId}/unlike`)
@@ -49,7 +85,15 @@ export const unlikeScream = (screamId) => dispatch => {
 export const deleteScream = (screamId) => dispatch => {
     axios.delete(`/scream/${screamId}`)
         .then(() => {
-            dispatch({type: DELETE_SCREAM, payload: screamId})
+            dispatch({
+                type: DELETE_SCREAM,
+                payload: screamId
+            })
         })
         .catch(err => console.log(err))
+}
+
+export const clearErrors = () => dispatch => {
+    dispatch({type: CLEAR_ERRORS})
+     
 }
