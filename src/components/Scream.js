0c283fb4c5468782c4726d23ee2cf4c248351014
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-09-26 11:24:24 +0100

    refactored like button add to individual scream

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import MyButton from '../util/MyButton';
 import DeleteScream from './DeleteScream';
 import ScreamDialog from './ScreamDialog';
+import LikeButton from './LikeButton';
 
 //dayjs
 import dayjs from 'dayjs';
@@ -16,16 +17,12 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 //material ui Icons
 import ChatIcon from '@material-ui/icons/Chat';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-import Favorite from '@material-ui/icons/Favorite';
+ 
 //redux
 import {
     connect
 } from 'react-redux';
-import {
-    likeScream,
-    unlikeScream
-} from '../redux/actions/dataActions';
+
 
 
 const styles = {
@@ -45,18 +42,7 @@ const styles = {
     }
 }
 class Scream extends Component {
-    likedScream = () => {
-        if (this.props.user.likes && this.props.user.likes.find(like => like.screamId === this.props.scream.screamId)) {
-            return true;
-        } else return false;
-    }
-    likeScream = () => {
-        this.props.likeScream(this.props.scream.screamId)
-    }
-    unlikeScream = () => {
-        this.props.unlikeScream(this.props.scream.screamId)
-    }
-
+  
     render() {
         dayjs.extend(relativeTime);
         const {
@@ -73,21 +59,7 @@ class Scream extends Component {
             user:{ authenticated, credentials: {handle} } 
 
         } = this.props;
-        const likeButton = !authenticated ? (
-            <MyButton tip="Like">
-                <Link to="/login">
-                    <FavoriteBorder color="primary" />
-                </Link>
-            </MyButton>
-        ): (
-            this.likedScream() ? (
-                <MyButton tip="Undo Like" onClick={this.unlikeScream}>
-                    <Favorite color="primary" />
-                </MyButton>
-            ):( <MyButton tip="Like" onClick={this.likeScream}>
-                    <FavoriteBorder color="primary" />
-            </MyButton>) 
-        );
+   
         const deleteButton = authenticated &&  userHandle === handle ?(
             //if authenticated and their own 'scream'
             <DeleteScream screamId={screamId}/>
@@ -118,7 +90,8 @@ class Scream extends Component {
                         <Typography variant = "body2" > 
                         {body} 
                         </Typography> 
-                        {likeButton} <span> {likeCount} Likes </span>
+                        <LikeButton screamId={screamId}/>
+                        <span> {likeCount} Likes </span>
                         <MyButton tip = "comments">
                             <ChatIcon color = "primary" / >
                         </MyButton> 
@@ -132,8 +105,6 @@ class Scream extends Component {
 }
 
 Scream.propTypes = {
-    likeScream: PropTypes.func.isRequired,
-    unlikeScream: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired,
     data: PropTypes.object.isRequired,
     classes: PropTypes.object.isRequired
@@ -143,9 +114,6 @@ const mapStateToProps = state => ({
     user: state.user,
     data: state.data
 });
-const mapActionsToProps = {
-    likeScream,
-    unlikeScream
-}
 
-export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Scream));
+
+export default connect(mapStateToProps)(withStyles(styles)(Scream));
--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -5,6 +5,7 @@ import React, {
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import MyButton from '../util/MyButton';
+import LikeButton from './LikeButton';
 import dayjs from 'dayjs';
 import {
     Link
@@ -13,13 +14,13 @@ import {
 //Material UI
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 // Icon
 import CloseIcon from '@material-ui/icons/Close';
 import UnfoldMore from '@material-ui/icons/UnfoldMore';
+import ChatIcon from '@material-ui/icons/Chat';
 //Redux
 import {connect} from 'react-redux';
 import {
@@ -43,6 +44,15 @@ const styles = theme =>  ({
     closeButton: {
         position: 'absolute',
         left: '90%'
+    },
+    expandButton: {
+        position: 'absolute',
+        left: '90%'
+    },
+    spinnerDiv: {
+        textAlign: 'center',
+        marginTop: 50,
+        marginBottom: 50
     }
 });
 class ScreamDialog extends Component {
@@ -80,7 +90,10 @@ class ScreamDialog extends Component {
         } = this.props;
 
         const dialogMarkup = loading ? (
-            <CircularProgress size={200} />
+            <div className={classes.spinnerDiv}>
+                   <CircularProgress size={200} thickness={2}/>
+            </div>
+         
         ): (
             <Grid container spacing={16}>
                 <Grid item sm={5}> 
@@ -92,10 +105,16 @@ class ScreamDialog extends Component {
                     </Typography>
                     <hr className={classes.invisibleSeperator}/>
                     <Typography variant="body2" color="secondary">
-                        {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
+                        {dayjs(createdAt).format('h:mm a, DD MMMM YYYY')}
                     </Typography>
                     <hr className={classes.invisibleSeperator}/>
                     <Typography variant="body1">{body}</Typography> 
+                    <LikeButton screamId={screamId}/>
+                    <span>{likeCount} likes</span>
+                    <MyButton tip = "comments">
+                            <ChatIcon color = "primary" / >
+                    </MyButton>                    
+                    <span> {commentCount} comments </span> 
                 </Grid> 
             </Grid>
         )
